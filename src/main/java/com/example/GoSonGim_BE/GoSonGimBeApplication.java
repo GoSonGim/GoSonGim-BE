695a97db1ffb8a2403a4d66b50081b3fbd59dc7e
고여경 <2yokyung@naver.com>
2025-10-19 18:09:40 +0900

    init

---
+++ src/main/java/com/example/GoSonGim_BE/GoSonGimBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.GoSonGim_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoSonGimBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoSonGimBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GoSonGim_BE/GoSonGimBeApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.GoSonGim_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+class GoSonGimBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
