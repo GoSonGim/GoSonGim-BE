674d6911c6332528b9d72d314ac41d0fb5339bb8
danhandev <danhandev@gmail.com>
2025-10-21 15:10:45 +0900

    refactor: auth 관련 예외 통합 처리

--- src/main/java/com/example/GoSonGim_BE/domain/auth/exception/EmailAlreadyUsedException.java
+++
@@ -1,9 +0,0 @@
-package com.example.GoSonGim_BE.domain.auth.exception;
-
-import com.example.GoSonGim_BE.global.exception.BaseException;
-
-public class EmailAlreadyUsedException extends BaseException {
-    public EmailAlreadyUsedException(String email) {
-        super("이미 등록된 이메일입니다: " + email, "EMAIL_ALREADY_USED");
-    }
-}
--- src/main/java/com/example/GoSonGim_BE/domain/auth/exception/UserDisabledException.java
+++
@@ -1,9 +0,0 @@
-package com.example.GoSonGim_BE.domain.auth.exception;
-
-import com.example.GoSonGim_BE.global.exception.BaseException;
-
-public class UserDisabledException extends BaseException {
-    public UserDisabledException() {
-        super("비활성화된 계정입니다.", "USER_DISABLED");
-    }
-}
